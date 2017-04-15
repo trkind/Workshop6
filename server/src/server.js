11abb6133f3887f8050ec84ef174beea6e28a0ba
Trent Kindvall <tgkindvall@umass.edu>
2017-04-15 09:57:07 -0400

    database

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,6 @@
 var express = require('express');
 // Creates an Express server.
 var app = express();
-var reverseMethod = require('./util');
-var reverseString = reverseMethod.reverseString;
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
@@ -11,8 +9,6 @@ app.use(bodyParser.text());
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-
 // You run the server from `server`, so `../client/build` is `server/../client/build`.
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
