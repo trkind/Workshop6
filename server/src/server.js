583efcbc3d3b71bd5259202e98ec3f9cec6705aa
Trent Kindvall <tgkindvall@umass.edu>
2017-04-15 09:49:49 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -7,25 +7,12 @@ var reverseString = reverseMethod.reverseString;
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    res.status(400).end()
-  }
-});
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
