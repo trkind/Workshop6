2fe5a0540c71ca2165480c4b69d2f64ad1e146b6
Trent Kindvall <tgkindvall@umass.edu>
2017-04-15 09:31:18 -0400

    express

---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+var reverseMethod = require('./util');
+var reverseString = reverseMethod.reverseString;
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
+
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  // If the request came with text, then the text() middleware handled it
+  // and made `req.body` a string.
+  // Check that req.body is a string.
+  if (typeof(req.body) === 'string') {
+    var reversed = reverseString(req.body);
+    res.send(reversed);
+  } else {
+    res.status(400).end()
+  }
+});
